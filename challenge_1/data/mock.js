d379e1d74341e9868aaa11a5fb9468189a58409f
Chen Wilson <chenlwilson@gmail.com>
2019-05-20 19:25:18 -0700

    Setting up

---
+++ challenge_1/data/mock.js
@@ -0,0 +1,24 @@
+const mockData = {
+  events: [
+    {
+      date: '-300', description: 'Pilgrims travel to the healing temples of Asclepieion to be cured of their ills. After a ritual purification the followers bring offerings or sacrifices.', lang: 'en', category1: 'By place', category2: 'Greece', granularity: 'year',
+    },
+    {
+      date: '-300', description: 'Pyrrhus, the King of Epirus, is taken as a hostage to Egypt after the Battle of Ipsus and makes a diplomatic marriage with the princess Antigone, daughter of Ptolemy and Berenice.', lang: 'en', category1: 'By place', category2: 'Egypt', granularity: 'year',
+    },
+    {
+      date: '-300', description: 'Ptolemy concludes an alliance with King Lysimachus of Thrace and gives him his daughter Arsinoe II in marriage.', lang: 'en', category1: 'By place', category2: 'Egypt', granularity: 'year',
+    },
+    {
+      date: '-300', description: 'Seleucus founds the city of Antioch, some 20 miles up the Orontes River, naming it after his father.', lang: 'en', category1: 'By place', category2: 'Seleucid Empire', granularity: 'year',
+    },
+    {
+      date: '-300', description: 'After the death of his wife Apama, Seleucus marries Stratonice, daughter of Demetrius Poliorcetes.', lang: 'en', category1: 'By place', category2: 'Seleucid Empire', granularity: 'year',
+    },
+    {
+      date: '-300', description: 'The central texts of Jainism, the Jain scriptures, are recorded (approximate date).{{Citation needed|date=March 2009}}', lang: 'en', category1: 'By place', category2: 'India', granularity: 'year',
+    },
+  ],
+};
+
+module.exports = mockData;
